7570eac870d4a9246ee822bb5cad3f1b91d46f31
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-08-01 21:50:35 +0530

    Create Print Like A Wave.java

---
+++ DAY 1/Print Like A Wave.java
@@ -0,0 +1,29 @@
+// https://www.naukri.com/code360/problems/print-like-a-wave_893268?leftPanelTabValue=PROBLEM
+
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+	public static int[] wavePrint(int arr[][], int nRows, int mCols) {
+		// Write your code here.
+		int[] result = new int[nRows * mCols] ;
+		int index = 0 ;
+
+		for(int col=0 ; col<mCols ; col++) {
+			if(col%2==0) {
+				for(int row = 0 ; row<nRows ; row++) {
+					result[index++] = arr[row][col] ;
+				}
+			}
+
+			else {
+				for(int row=nRows-1 ; row>=0 ; row--) {
+					result[index++] = arr[row][col] ;
+				}
+			}
+		}
+		return result ;
+	}
+}
+
+
